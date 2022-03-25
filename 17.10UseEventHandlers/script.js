6eb46be18626b941d65d2126062a223e215e60a2
LHL-source <97887598+LHL-source@users.noreply.github.com>
2022-03-25 12:13:00 +0100

    Add files via upload

---
+++ 17.10UseEventHandlers/script.js
@@ -0,0 +1,22 @@
+/*part1*/
+var buttonClicked = document.querySelector('#mybutton');
+buttonClicked;
+
+buttonClicked.addEventListener("click", function(e) {
+   alert("button clicked");
+});
+
+/*part 2*/
+/*var buttonColor = document.querySelector('#mybutton_bg');
+buttonColor;
+
+buttonColor.addEventListener("click", function(bodyBackgrColor) {
+   console.log(bodyBackgrColor);
+});
+
+color = function(bodyBackgrColor){
+   .btnbg_alert{
+      background-color: red;
+      };
+
+};*/
