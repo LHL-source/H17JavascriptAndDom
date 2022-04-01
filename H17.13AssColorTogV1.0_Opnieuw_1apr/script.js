fa59c3f97a865c676898204a8d24b326c963a735
LHL-source <97887598+LHL-source@users.noreply.github.com>
2022-04-01 13:29:27 +0200

    Add files via upload

--- H17.13AssColorTogV1.0_Opnieuw_1apr/script.js
+++ H17.13AssColorTogV1.0_Opnieuw_1apr/script.js
@@ -1,37 +1,58 @@
 /* alleen hamburger zichtbaar op scherm menu-kleur niet via css display=none workd ? y*/
-document.addEventListener('click', function () {
+ /*stap 1 maak een function toggle*/
+ 
+function menuFunction(){
+    let menuKleur =document.querySelector('.menuKleur');
+    console.log("menuFunction = " +  menuKleur);
+    menuKleur,classList.toggle('menuToggle');
+
+}
+
+ /*function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("mystyle");
+ }*/
+
+document.addEventListener('click', function(menuFunction) {
     console.log('hamburgerMenu/eventListener klik gelukt');
     let menuKleurOpen=document.querySelector('.menuKleur');
     console.log('hamburger/function/menuKleuropen' + menuKleurOpen);
 
-    /*na klik menu-kleur komt te voorschijn works ? y*/
-    let getMenuKleur = document.querySelector('.menuKleur');
-    console.log('ophalen getMenuKleur= ' + getMenuKleur );
 
-    let getMenuKleurUlDisplay = document.querySelector('.menuKleur.ul.display');
-    console.log('ophalen getMenuKleurUlDisplay= ' + getMenuKleurUlDisplay );
+})/*addEventListener*/
 
-
-    let getMenuOpen = document.querySelector('.menuOpen');
-    console.log('ophalen getMenuOpen= ' + getMenuOpen );
-    
-    let getmenuClose =  document.querySelector('.menuClose');
-    console.log('ophalengetmenuClose= ' + getmenuClose );
-    
-   /*indien menukleur is display=none dan zet display=block, else if display=block*/ 
-   if ( getMenuKleur = 'display: none'){
-        console.log("getMenukleur: " + getMenuKleur );
-        getMenuKleur.classList.replace('getmenuClose', 'getMenuOpen');
-        console.log("if getMenukleur = none dan doet display = block" + getMenuKleur.classList);
-    }
-
-    else if (getMenuKleur = 'display: block'){
-         getMenuKleur.classList.replace(getmenuOpen, getMenuClose);
-         console.log("if getMenukleur = none dan doet display = block" + getMenuKleur.classList);
-    };
-    
+   
          
         
-
+/* hieronder kladblok*/
     
-})/*addEventListener*/
+
+
+
+ /*na klik menu-kleur komt te voorschijn works ? y*/
+   // let getMenuKleur = document.querySelector('.menuKleur');
+   // console.log('ophalen getMenuKleur= ' + getMenuKleur );
+
+   // let getMenuKleurUlDisplay = document.querySelector('.menuKleur.ul.display');
+  //  console.log('ophalen getMenuKleurUlDisplay= ' + getMenuKleurUlDisplay );
+
+
+   // let getMenuOpen = document.querySelector('.menuOpen');
+  //  console.log('ophalen getMenuOpen= ' + getMenuOpen );
+
+  //  let getmenuClose =  document.querySelector('.menuClose');
+  //  console.log('ophalengetmenuClose= ' + getmenuClose );
+    
+   /*indien menukleur is display=none dan zet display=block, else if display=block*/ 
+  // if ( getMenuKleur = 'display: none'){
+   //     console.log("getMenukleur: " + getMenuKleur );
+  //      getMenuKleur.classList.replace('getmenuClose', 'getMenuOpen');
+  //      console.log("if getMenukleur = none dan doet display = block" + getMenuKleur.classList);
+ //   }
+
+  //  else if (getMenuKleur = 'display: block'){
+  //       getMenuKleur.classList.replace(getmenuOpen, getMenuClose);
+  //       console.log("if getMenukleur = none dan doet display = block" + getMenuKleur.classList);
+ //   };
+    
+         
