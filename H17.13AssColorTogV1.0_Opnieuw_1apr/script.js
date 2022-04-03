a0308b7e09bd2341371f189dc128894cc5d120e2
LHL-source <97887598+LHL-source@users.noreply.github.com>
2022-04-03 09:39:26 +0200

    Add files via upload

--- H17.13AssColorTogV1.0_Opnieuw_1apr/script.js
+++ H17.13AssColorTogV1.0_Opnieuw_1apr/script.js
@@ -4,55 +4,70 @@
 function menuFunction(){
     let menuKleur =document.querySelector('.menuKleur');
     console.log("menuFunction = " +  menuKleur);
-    menuKleur,classList.toggle('menuToggle');
+    menuKleur.classList.toggle('visible');
 
 }
 
- /*function myFunction() {
-    var element = document.getElementById("myDIV");
-    element.classList.toggle("mystyle");
- }*/
+ /*stap 2 maak van menuKleur visible of invisible*/
+let btnToggleNav = document.querySelector('.btn-toggle-nav')
 
-document.addEventListener('click', function(menuFunction) {
+btnToggleNav.addEventListener('click', function() {
+    menuFunction();
     console.log('hamburgerMenu/eventListener klik gelukt');
-    let menuKleurOpen=document.querySelector('.menuKleur');
-    console.log('hamburger/function/menuKleuropen' + menuKleurOpen);
-
-
-})/*addEventListener*/
-
    
+    })/*addEventListener*/
+
+
+
+
+/*functie: klik op button home-grey en body wordt grijs works ? y*/
+
+var getBtnHomeGrey = document.querySelector('.home-grey');
+console.log("getBtnHomeGrey : " + getBtnHomeGrey);
+
+getBtnHomeGrey = addEventListener('click' , function(){
+  
+        var getBody = document.getElementById('myLichaam');
+        console.log(" hallo body :" + getBody);
+        getBody.classList.add('kleurGrijs');
+        console.log("getBody.classList = " + getBody.classList);
+
+   /*close the menu*/
+    menuFunction();
+    
+   
+   /* })/*addEventListener*/
+
+}) ; /*getBtnHomeGrey*/   
+
+
+/*button Red*/
+//var getBtnRed = document.querySelector('.red');
+//console.log("getBtnRed : " + getBtnRed);
+
+/* stap 2functie: klik op button:red en body wordt rood works ?*/
+     
+     
+    /* getBtnRed = addEventListener('click' , function(){
+       
+             var getBody = document.querySelector('body');
+             console.log(" hallo body :" + getBody);
+             getBody.classList.add('kleurRood');
+             console.log("getBody = "+ getBody.classList);
+     
+        /*close the menu*/
+        /* menuFunction();*/
          
         
+        /* })/*addEventListener*/
+     
+    /* });  */ /*getBtnHomeGrey*/   
+     
+
+
 /* hieronder kladblok*/
     
 
 
 
- /*na klik menu-kleur komt te voorschijn works ? y*/
-   // let getMenuKleur = document.querySelector('.menuKleur');
-   // console.log('ophalen getMenuKleur= ' + getMenuKleur );
-
-   // let getMenuKleurUlDisplay = document.querySelector('.menuKleur.ul.display');
-  //  console.log('ophalen getMenuKleurUlDisplay= ' + getMenuKleurUlDisplay );
-
-
-   // let getMenuOpen = document.querySelector('.menuOpen');
-  //  console.log('ophalen getMenuOpen= ' + getMenuOpen );
-
-  //  let getmenuClose =  document.querySelector('.menuClose');
-  //  console.log('ophalengetmenuClose= ' + getmenuClose );
-    
-   /*indien menukleur is display=none dan zet display=block, else if display=block*/ 
-  // if ( getMenuKleur = 'display: none'){
-   //     console.log("getMenukleur: " + getMenuKleur );
-  //      getMenuKleur.classList.replace('getmenuClose', 'getMenuOpen');
-  //      console.log("if getMenukleur = none dan doet display = block" + getMenuKleur.classList);
- //   }
-
-  //  else if (getMenuKleur = 'display: block'){
-  //       getMenuKleur.classList.replace(getmenuOpen, getMenuClose);
-  //       console.log("if getMenukleur = none dan doet display = block" + getMenuKleur.classList);
- //   };
-    
-         
+ 
